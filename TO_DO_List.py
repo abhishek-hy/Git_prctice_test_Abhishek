aa1723b66a6ce033d806cc0c35755b9d692ba5b1
abhishek-hy <tabishek233@gmail.com>
2024-03-25 11:52:20 +0530

    first commit

---
+++ TO_DO_List.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def add():
+    task = entry.get()
+    if task :
+        listbox.insert(tk.END,task)
+        entry.delete(0,tk.END)
+
+    else:
+        messagebox.showwarning("Warning !, Please add task")
+
+def delete():
+    try:
+        select_index = listbox.curselection()
+        listbox.delete(select_index)
+
+    except:
+        messagebox.showwarning("Warning !, Please select task")    
+
+window = tk.Tk()
+window.title("TO DO LIST")
+
+entry = tk.Entry(window,width=30)
+entry.pack()
+
+Button_add = tk.Button(window,text="Add Task",command=add)
+Button_add.pack()
+Button_del = tk.Button(window,text="Delete Task",command=delete)
+Button_del.pack()
+listbox = tk.Listbox(window,selectmode=tk.SINGLE,width=40,height=10)
+listbox.pack()
+
+
+window.mainloop()
